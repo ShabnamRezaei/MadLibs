0b6277d66251038af6eddc703eb6616c36a88ae5
Shabnam Rezaei <shrezaei6@gmail.com>
2023-01-02 15:58:19 +0100

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# My projects
+# Project 1
+# Mad Libs
+# Continents (Asia, Africa, Europe, North America, South America, Oceania and Antarctica)
+
+continent1 = input("continent: ")
+continent2 = input("continent: ")
+continent3 = input("continent: ")
+continent4 = input("continent: ")
+continent5 = input("continent: ")
+continent6 = input("continent: ")
+continent7 = input("continent: ")
+
+madlibs = f"The Earth consists of 71% and 29% of landmasses. These landmasses are further divided into continents. \
+These are {continent1}, {continent2}, {continent3}, {continent4}, {continent5}, {continent6} and {continent7}."
+
+print(madlibs)
