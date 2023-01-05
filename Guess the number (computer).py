6c16f407613be6201ddb34512fbcee7ca930b354
Shabnam Rezaei <shrezaei6@gmail.com>
2023-01-05 13:25:13 +0100

    Update

--- Guess the number (computer).py
+++ Guess the number (computer).py
@@ -1,15 +1,15 @@
 import random
 
-def guess(x):
+def guess(x):  # Defining a function
     random_number = random.randint (1, x)
-    guess = 0
+    guess = 0  # To initialize and tell the coputer that this number exists, but it is not in the range between 1 and x.
     while guess != random_number:
-        guess = int(input(f'guess a number between 1 and {x}: '))
+        guess = int(input(f'guess a number between 1 and {x}: '))  # Notice one space after x
         if guess < random_number:
             print('Sorry! guess again, too low.')
-        elif guess > random_number:
+        elif guess > random_number:  
             print('Sorry, guess again, too high.')
-
+# One empty line here
     print(f'Congrats, you have guessed the number {random_number}')
-
+# One empty line here
 guess(10)
--- Guess the number (user).py
+++ Guess the number (user).py
@@ -7,15 +7,15 @@ def guess(x):
     while feedback != 'c':
         if low != high:
             guess = random.randint(low, high)
-        else:
+        else: # otherwise (low = high)
             guess = low  # Can also be high
         feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?') . lower()
         if feedback == 'h':
             high = guess - 1
         elif feedback == 'l':
-            high = guess + 1
+            low = guess + 1
 
     print(f'Congrats, the computer have guessed the number correctly {guess}')
 
-guess(10)
+guess(100)
 
